93bdcc3a19cab9d4525c8bfa5dcc07bb6b9f6a38
Michal Papuga <mpapuga@prescientco.com>
2019-11-24 17:10:26 +0100

    Complete EX 0

--- FirstClasses/CompileModule/intro_ex1.erl
+++ FirstClasses/CompileModule/intro_ex1.erl
@@ -1,9 +1,10 @@
 -module(intro_ex1).
 
 -export([hello_name/1]).
+-export([hello/2]).
 
 hello_name(Name) ->
     io:fwrite("Hello ~p!\n", [Name]).
 
 hello(Name, Surname) ->
-    io:fwrite("Hello ~p!\n", [Name]).
+    io:fwrite("Hello ~p ~p!\n", [Name, Surname]).
