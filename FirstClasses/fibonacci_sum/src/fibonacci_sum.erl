3761000c43aaccc781eb770e9fcc05d87489ccab
Michal Papuga <mpapuga@prescientco.com>
2019-11-24 20:09:22 +0100

    Complete EX 5.

--- FirstClasses/fibonacci_sum/src/fibonacci_sum.erl
+++ FirstClasses/fibonacci_sum/src/fibonacci_sum.erl
@@ -7,4 +7,11 @@ fibonacci_sum() ->
     fibonacci_sum(1, 1, 0, Limit).
 
 fibonacci_sum(_Prev, _Curr, _Sum, _Limit) ->
-    1.
+    case _Sum of
+        _Sum when _Curr =< _Limit andalso _Curr rem 2 == 0 ->
+            _Curr + fibonacci_sum(_Curr, _Prev + _Curr, _Sum + 1, _Limit);
+        _Sum when _Curr =< _Limit ->
+            fibonacci_sum(_Curr, _Prev + _Curr, _Sum + 1, _Limit);
+        _ ->
+            0
+        end.
