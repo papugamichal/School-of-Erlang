d5e7aa4c33e6ed2f4e25f70c7a9dfc40ca057b34
Michal Papuga <mpapuga@prescientco.com>
2019-11-28 21:30:58 +0100

    Complete Ex 2.1 and Ex 2.2

--- SecondClasses/gcd/src/gcd.erl
+++ SecondClasses/gcd/src/gcd.erl
@@ -3,12 +3,16 @@
 -export([gcd/2]).
 
 gcd(A, B) ->
-    case_gcd(A, B). % comment this line
-    % head_match_gcd(A, B). % uncomment this line for the head matching exercise
+    %case_gcd(A, B). % comment this line
+    head_match_gcd(A, B). % uncomment this line for the head matching exercise
 
-case_gcd(A, B) ->
-    R = A rem B,
-    case R of
-        0 -> B;
-        _ -> case_gcd(B, R)
-    end.
+% case_gcd(A, B) ->
+%     R = A rem B,
+%     case R of
+%         0 -> B;
+%         _ -> case_gcd(B, R)
+%     end.
+
+head_match_gcd(A, 0) -> A;
+head_match_gcd(A, B) ->
+    head_match_gcd(B, A rem B).
--- SecondClasses/rev_polish_notation/src/rev_polish_notation.erl
+++ SecondClasses/rev_polish_notation/src/rev_polish_notation.erl
@@ -2,6 +2,18 @@
 
 -export([evaluate/1]).
 
+evaluate(List)->
+    R = lists:foldl(fun next/2, [],List),
+    lists:last(R).
 
-evaluate(_) ->
-    1.
+next(Elem, Stack) when is_integer(Elem) ->
+    [Elem | Stack];
+next(Elem, Stack) when is_function(Elem) andalso length(Stack) >= 2 ->
+    {Last, Remaining} = get_last_and_ramaining(Stack),
+    {Last1, Remaining1} = get_last_and_ramaining(Remaining),
+    [Elem(Last, Last1) | Remaining1];
+next(Elem, Stack) when is_function(Elem) ->    
+    {false, "Error!"}.
+
+get_last_and_ramaining(List = [H|T]) ->
+    {H, T}.
