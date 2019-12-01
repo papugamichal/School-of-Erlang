a0e48521774b15fde145f4f9df8c4c9757bb0f51
Michal Papuga <mpapuga@prescientco.com>
2019-12-01 08:24:19 +0100

    Cleanup code.

--- SecondClasses/gcd/src/gcd.erl
+++ SecondClasses/gcd/src/gcd.erl
@@ -6,12 +6,12 @@ gcd(A, B) ->
     %case_gcd(A, B). % comment this line
     head_match_gcd(A, B). % uncomment this line for the head matching exercise
 
-% case_gcd(A, B) ->
-%     R = A rem B,
-%     case R of
-%         0 -> B;
-%         _ -> case_gcd(B, R)
-%     end.
+case_gcd(A, B) ->
+    R = A rem B,
+    case R of
+        0 -> B;
+        _ -> case_gcd(B, R)
+    end.
 
 head_match_gcd(A, 0) -> A;
 head_match_gcd(A, B) ->
--- SecondClasses/rev_polish_notation/src/rev_polish_notation.erl
+++ SecondClasses/rev_polish_notation/src/rev_polish_notation.erl
@@ -3,17 +3,27 @@
 -export([evaluate/1]).
 
 evaluate(List)->
-    R = lists:foldl(fun next/2, [],List),
+    R = lists:foldl(fun next/2, [], List),
     lists:last(R).
 
 next(Elem, Stack) when is_integer(Elem) ->
     [Elem | Stack];
-next(Elem, Stack) when is_function(Elem) andalso length(Stack) >= 2 ->
-    {Last, Remaining} = get_last_and_ramaining(Stack),
-    {Last1, Remaining1} = get_last_and_ramaining(Remaining),
-    [Elem(Last, Last1) | Remaining1];
-next(Elem, Stack) when is_function(Elem) ->    
-    {false, "Error!"}.
+next(Elem, Stack) when is_function(Elem)->
+    case get_fun_args_number(Elem) of
+        Arity when Arity == 2 andalso length(Stack) >= 2 ->
+            {Last, Remaining} = get_stack_head_and_ramaining(Stack),
+            {Last1, Remaining1} = get_stack_head_and_ramaining(Remaining),
+            [Elem(Last, Last1) | Remaining1];
+        Arity when Arity == 1 andalso length(Stack) >= 1 ->
+            {Last, Remaining} = get_stack_head_and_ramaining(Stack),
+            [Elem(Last) | Remaining];
+        Arity when Arity >= 1 andalso length(Stack) >= 0 ->
+            {false, "Got function but there is not enough  arguments!"}
+    end.
 
-get_last_and_ramaining(List = [H|T]) ->
+get_fun_args_number(Function) ->
+    {_, Number} = erlang:fun_info(Function,arity),
+    Number.
+
+get_stack_head_and_ramaining([H|T]) ->
     {H, T}.
